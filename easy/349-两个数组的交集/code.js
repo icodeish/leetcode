18cf2bfc8c8f77d9dcb0410795b1573e258e3d23
youshaofeng <283169382@qq.com>
2022-01-23 17:03:46 +0800

    349-两个数组的交集

--- easy/349-两个数组的交集/code.js
+++ easy/349-两个数组的交集/code.js
@@ -2,14 +2,18 @@
 var nums1=[4,9,5]
 var nums2=[9,4,9,8,4]
 var intersection = function(nums1, nums2) {
-    let arr=[]
-    for(let i=0;i<nums1.length;i++){
-       
-           if(nums2.indexOf(nums1[i])){
-               arr.push(nums1[i])
-           }
-  
-        return arr
+    let set1=new Set(nums1)
+    let set2=new Set(nums2)
+    if(set1.size<set2.size){
+        [set1,set2]=[set2,set1]
     }
+    const set3= new Set()
+    for (item of set2){
+        if(set1.has(item)){
+            set3.add(item)
+        }
+    }
+    return [...set3]
+  
 };
 console.log(intersection(nums1,nums2))
