ff021901f42b870633cb8a0167b58928b34f90e8
尤少峰 <283169382@qq.com>
2022-01-24 10:30:57 +0800

    加一

--- easy/66-加一/code.js
+++ easy/66-加一/code.js
@@ -1,17 +1,17 @@
 //解法1
-var digits=[9,9,9]
+var digits=[9,9,9,9,9,9,9]
 
 var plusOne = function(digits) {
- if(digits[digits.length-1]===9){
-    let num =parseInt(digits.toString().split(",").join(""))+1
-    digits= num.toString().split("")
-    return digits
- }else{
-     var lastIndex=parseInt(digits[digits.length-1])+1
-     digits[digits.length-1]=lastIndex
-     return digits
- }
+    for(let i=digits.length-1;0<=i;i--){
+        if(digits[i]!==9){
+            digits[i]++
+            return digits
+        }else{
+            digits[i]=0
+        }
 
-    
+    }
+    digits.unshift(1)
+    return digits
 };
 console.log(plusOne(digits))
