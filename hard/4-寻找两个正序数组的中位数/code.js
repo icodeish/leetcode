02802b79322a7f900f3fc65b86cf9cfb49ffc808
尤少峰 <283169382@qq.com>
2022-02-14 14:08:34 +0800

    4-寻找两个正序数组的中位数

---
+++ hard/4-寻找两个正序数组的中位数/code.js
@@ -0,0 +1,14 @@
+//解法1
+let nums1 = [1, 2, 3]
+let nums2 = [4]
+var findMedianSortedArrays = function (nums1, nums2) {
+    let new_arr = nums1.concat(nums2)
+    new_arr.sort((a, b) => {
+        return a - b
+    })
+    if (new_arr.length%2 === 0)
+        return (new_arr[parseInt(new_arr.length / 2)] + new_arr[parseInt(new_arr.length / 2) - 1]) / 2
+        return new_arr[parseInt(new_arr.length / 2)] 
+
+};
+console.log(findMedianSortedArrays(nums1, nums2))
