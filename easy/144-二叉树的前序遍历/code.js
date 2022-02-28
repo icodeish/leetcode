0a43d4e58ada654953954ed1c92478b7f5cacd16
尤少峰 <283169382@qq.com>
2022-02-28 14:17:29 +0800

    144-二叉树的前序遍历

---
+++ easy/144-二叉树的前序遍历/code.js
@@ -0,0 +1,33 @@
+let root={
+    val:1,
+    left:{
+        val:2,
+        left:{
+            val:4,
+            left:null,
+            right:null
+        },
+        right:{
+            val:5,
+            left:null,
+            right:null
+        }
+    },
+    right:{
+        val:3,
+        left:null,
+        right:null
+    }
+}
+function preorderTraversal(root){
+    let res=[]
+    preorder(root)
+    function preorder(root){
+        if(!root) return
+        res.push(root.val)
+        preorder(root.left)
+        preorder(root.right)
+    }
+    return res
+}
+console.log(preorderTraversal(root))
