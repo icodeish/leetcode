d3821aef9e3697952182299d34d156a42385250c
尤少峰 <283169382@qq.com>
2022-02-28 13:39:15 +0800

    二叉树的中序列遍历

---
+++ easy/94-二叉树的中序遍历/code.js
@@ -0,0 +1,34 @@
+
+let root={
+    val:1,
+    left:{
+        val:2,
+        left:{
+            val:4,
+            left:null,
+            right:null
+        },
+        right:{
+            val:5,
+            left:null,
+            right:null
+        }
+    },
+    right:{
+        val:3,
+        left:null,
+        right:null
+    }
+}
+function inorderTraversal(root){
+    let res=[]
+    inorder(root)
+    function inorder (root){
+        if(!root) return 
+        inorder(root.left)
+        res.push(root.val)
+        inorder(root.right)
+    }
+    return res
+}
+console.log(inorderTraversal(root))
