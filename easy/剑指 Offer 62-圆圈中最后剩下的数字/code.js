730e23e33b87ba377a8bf71a066c26cda19a5c74
youshaofeng <283169382@qq.com>
2022-03-18 14:47:32 +0800

    update

--- easy/剑指 Offer 62-圆圈中最后剩下的数字/code.js
+++ easy/剑指 Offer 62-圆圈中最后剩下的数字/code.js
@@ -1,10 +1,9 @@
 const n = 5, m = 3
+// 0 1  3 4 
+// 3
 function lastRemaining(n, m) {
-    let arr = Array.from({ length: 5 }).map((item, index) => {
-        return index
-    })
-    
+    let res = 0
+    for (let i = 2; i <= n; i++)
+        res = (res + m) % i
+    return res
 }   
-let arr=[0,1,2,3,4]
-console.log(arr.splice(2,1))
-console.log(arr)
