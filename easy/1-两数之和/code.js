ea041a26c275cbdc901ba4992299d9964923138f
尤少峰 <283169382@qq.com>
2022-06-29 14:50:14 +0800

    1-两数之和

--- easy/1-两数之和/code.js
+++ easy/1-两数之和/code.js
@@ -1,15 +1,17 @@
+let nums = [1, 2, 3, 4]
+let target = 4
+
 //解法1
-let nums=[1,2,3,4]
-let target=4 
-function twoNum(nums,target){
-    let arr=[]
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                arr.push(i,j)
-                return arr
+
+function twoSum(nums, target) {
+    let res = []
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                res.push(i, j)
+                return res
             }
         }
     }
 }
-console.log(twoNum(nums,target))
+console.log(twoSum(nums,target))
